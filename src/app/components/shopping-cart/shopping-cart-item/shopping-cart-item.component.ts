d8d8789033bff6c509d0889651b6b3d1447114c0
Romesh <romesh@medoment.com>
2024-12-25 14:47:04 +0530

    1

--- src/app/components/shopping-cart/shopping-cart-item/shopping-cart-item.component.ts
+++ src/app/components/shopping-cart/shopping-cart-item/shopping-cart-item.component.ts
@@ -3,13 +3,22 @@ import { ShoppingCartItem } from '../../../models/shopping-cart-item.model';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ShoppingCartService } from '../../../services/shopping-cart.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-shopping-cart-item',
   templateUrl: './shopping-cart-item.component.html',
   styleUrls: ['./shopping-cart-item.component.css'],
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [
+    FormsModule,
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatChipsModule,
+  ],
 })
 export class ShoppingCartItemComponent {
   @Input() cartItem?: ShoppingCartItem;
